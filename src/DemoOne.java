1748ec72b6d3301f4cc7f6ff69cb0b1f38ae9bae
someshhanji26@gmail.com <someshhanji26@gmail.com>
2025-05-24 16:22:59 +0530

    printing statements

--- src/DemoOne.java
+++ src/DemoOne.java
@@ -1,3 +1,8 @@
-//Just demo purpose
+//Just demogit  purpose
 public class DemoOne {
+    //add code to testing purpose
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
--- src/DemoSecond.java
+++ src/DemoSecond.java
@@ -1,2 +1,6 @@
 public class DemoSecond {
+    //add code to testing purpose
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
